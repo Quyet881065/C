8930f17f12646ac7ebe3d11b5a483c5ff74cee99
Quyet881065 <123087180+Quyet881065@users.noreply.github.com>
2024-01-24 23:49:28 -0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b, sum;
+  printf("a= ");
+  scanf("%d", &a);
+  printf("b= ");
+  scanf("%d", &b);
+  sum = a+b;
+  printf("sum= %d",sum);
+  return 0 ;
+}
